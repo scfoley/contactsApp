9332af9912bb12a69e788bcb7e5c2c9383c652a2
Shane Foley <Shane.Foley@neudesic.com>
2019-02-19 18:55:08 -0700

    base resources generated

---
+++ ContactsApp/ContactsApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PropertyChanged;
+using System.Threading.Tasks;
+
+namespace ContactsApp.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public abstract class BaseViewModel
+    {
+        public virtual Task OnAppearing() { return Task.CompletedTask; }
+        public virtual Task OnDisappearing() { return Task.CompletedTask; }
+    }
+}
---
+++ ContactsApp/ContactsApp/Views/BaseView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ContactsApp.ViewModels;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContactsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BaseView : ContentPage
+    {
+		public BaseView()
+		{
+			InitializeComponent ();
+		}
+
+        private async void OnAppearing(object sender, EventArgs e)
+            => await (BindingContext as BaseViewModel)?.OnAppearing();
+
+        private async void OnDisappearing(object sender, EventArgs e)
+            => await (BindingContext as BaseViewModel)?.OnDisappearing();
+    }
+}
