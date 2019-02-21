367d119302a4cd0cade276220660c867fa2dc49b
Shane Foley <Shane.Foley@neudesic.com>
2019-02-20 17:27:38 -0700

    ContactService class added. Implementation needed.

---
+++ ContactsApp/ContactsApp/Services/ContactService.cs
@@ -0,0 +1,36 @@
+﻿using ContactsApp.Models;
+using ContactsApp.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Services
+{
+    public class ContactService : IContactService
+    {
+        private readonly IPlatformContactService _platformContactService;
+
+        public ContactService(IPlatformContactService platformContactService)
+        {
+            _platformContactService = platformContactService;
+        }
+
+        public List<Contact> LoadedContacts { get; set; }
+
+        public Task<List<ContactList>> GetContactsGroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact LoadFavoriteContact()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveFavoriteContact(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
