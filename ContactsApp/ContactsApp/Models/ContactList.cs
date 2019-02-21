8863a85b11c4677585d0acd9be7c59999bbc171e
Shane Foley <Shane.Foley@neudesic.com>
2019-02-20 17:25:15 -0700

    Contact service outlined

---
+++ ContactsApp/ContactsApp/Models/ContactList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ContactsApp.Models
+{
+    public class ContactList : List<Contact>
+    {
+        public string Heading { get; set; }
+    }
+}
---
+++ ContactsApp/ContactsApp/Services/Contracts/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using ContactsApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Services.Contracts
+{
+    public interface IContactService
+    {
+        List<Contact> LoadedContacts { get; set; }
+        void SaveFavoriteContact(Contact contact);
+        Contact LoadFavoriteContact();
+        Task<List<ContactList>> GetContactsGroups();
+    }
+}
